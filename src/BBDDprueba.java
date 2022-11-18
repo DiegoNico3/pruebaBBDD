acb0ad9f8c2bc2036088902dfe7c82aae6198281
DiegoNico3 <diegonicolasmorenosaldivar@gmail.com>
2022-11-18 08:39:06 -0300

    si

--- src/BBDDprueba.java
+++ src/BBDDprueba.java
@@ -1,6 +1,6 @@
 import javax.swing.*;
 import java.sql.*;
-public class BBDDprueba {//dumb
+public class BBDDprueba {
     public static void main(String[] args) {
         Connection con = null;
         String sURL = "jdbc:mysql://localhost/bbdd_prueba";
@@ -13,21 +13,39 @@ public class BBDDprueba {//dumb
             //stmt.execute("create database lineadecodigo");
             //System.out.println("Base de datos creada correctamente");
 
-//            PreparedStatement pstm = con.prepareStatement("insert into cuenta values(?,?,?)");
-//            pstm.setString(1,"12345");
+//            PreparedStatement pstm = con.prepareStatement("insert into cuenta values(?,?,?,?,?,?,null,null,null)");
+//            pstm.setString(1,"Ricardo Adorno");
+//            pstm.setString(2,"4872651");
+//            pstm.setString(3,"1234");
+//            pstm.setString(4,"1345000");
+//            pstm.setString(5,"45757200542581423");
+//            pstm.setString(6,"12/24");
+//            pstm.setString(7,"12345");
+//            pstm.setString(8,"12345");
+//            pstm.setString(9,"12345");
 //            pstm.setString(2,"Ricardo Adorno, cerrista de corazon");
 //            pstm.setString(3,"1000000");
 //            pstm.executeUpdate();
-//
-//
-//            System.out.println("Registro creado correctamente");
 
-            PreparedStatement pstm = con.prepareStatement("select * from cuenta where numCuenta = 12345 ");
+
+            //System.out.println("Registro creado correctamente");
+
+            PreparedStatement pstm = con.prepareStatement("select * from cuenta where PinCuenta = 1234 ");
             ResultSet rs = pstm.executeQuery();
+
+
+
+
             while(rs.next()){
-                System.out.println("Numero de cuenta: "+rs.getString("numCuenta"));
+                System.out.println("Numero de cuenta: "+rs.getString("NumCuenta"));
                 System.out.println("Nombre: "+rs.getString("Nombre"));
                 System.out.println("Saldo: "+rs.getString("Saldo"));
+                System.out.println("Pin de cuenta: "+rs.getString("PinCuenta"));
+                System.out.println("Numero de Tarjeta: "+rs.getString("NumTarjeta"));
+                System.out.println("Fecha de vencimiento: "+rs.getString("FechaValid"));
+                System.out.println("Numero Secreto: "+rs.getString("NumSecreto"));
+                System.out.println("Cedula de identidad: "+rs.getString("CI"));
+                System.out.println("Contrasenha: "+rs.getString("Password"));;
                 System.out.println();
             }
 
